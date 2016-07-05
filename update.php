74ea4301ff03a298c5cf115265a1b01a61799f3d
kbakkes <karim.bakkes@gmail.com>
2016-07-05 18:33:30 +0200

    Updated update.php
    Kan nu ook de wave aanpassen

--- update.php
+++ update.php
@@ -2,6 +2,8 @@
 $id = $_GET['id'];
 $name = $_GET['name'];
 $score = $_GET['score'];
+$wave = $_GET['wave'];
+$waveID = $_GET['waveID'];
 ?>
 
 <!DOCTYPE html>
@@ -16,24 +18,29 @@ $score = $_GET['score'];
 <form class="container container-table" action="" method="post">
     <h4>Name:</h4>
     <input type="text" name="name" value="<?=$name?>">
+    <h4>Wave:</h4>
+    <input type="text" name="wave" value="<?=$wave?>">
     <h4>Score</h4>
     <input type="text" name="score" value="<?=$score?>">
     <br><br>
     <input type="submit" name="submit" value="Edit">
     </form>
+    
 <?php
 
 if(isset($_POST['submit'])) {
-
+    
     $new_name = htmlspecialchars($_POST['name']);
     $new_score = htmlspecialchars($_POST['score']);
-
+    $new_wave = htmlspecialchars($_POST['wave']);
 
     $highscoreConnect = mysqli_connect("localhost", "root", "", "plasticats");
 
     $sql_update = "UPDATE `highscores` SET `name` = '{$new_name}', `scores` = '{$new_score}' WHERE `highscores`.`id` = {$id}";
-
+    $wave_update = "UPDATE `wavescore` SET `wave` = '{$new_wave}' WHERE `wavescore`.`id` = {$waveID}";
+    
     mysqli_query($highscoreConnect,$sql_update);
+    mysqli_query($highscoreConnect,$wave_update);
 
     header("Location: succes.php");
 }
