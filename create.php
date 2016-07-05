3302c66957a45f98540166c84b0db5b706290971
kbakkes <karim.bakkes@gmail.com>
2016-07-05 18:38:07 +0200

    create nieuwe row met wave erbij

--- create.php
+++ create.php
@@ -10,7 +10,9 @@
 
         <h4>Name:</h4>
         <input type="text" name="name" value="Name">
-        <h4>Score</h4>
+        <h4>Wave:</h4>
+        <input type="text" name="wave" value="Wave">
+        <h4>Score:</h4>
         <input type="text" name="score" value="Score">
         <br><br>
         <input type="submit" class="btn" name="submit" value="Create">
@@ -21,19 +23,18 @@
 
             $new_name = htmlspecialchars($_POST['name']);
             $new_score = htmlspecialchars($_POST['score']);
+            $new_wave = htmlspecialchars($_POST['wave']);
 
             $highscoreConnect = mysqli_connect("localhost", "root", "", "plasticats");
 
             $sql_create = "INSERT INTO `highscores` (`id`, `name`, `scores`) VALUES (NULL, '{$new_name}', '{$new_score}')";
+            $wave_create = "INSERT INTO `wavescore` (`id`, `score`, `wave`) VALUES (NULL, '{$new_score}', '{$new_wave}')";
 
             mysqli_query($highscoreConnect,$sql_create);
+            mysqli_query($highscoreConnect,$wave_create);
 
             header("Location: succes.php");
         }
     ?>
-
-
-
-
 </body>
 </html>
